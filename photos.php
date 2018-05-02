4214c6c971f822bd8cc0fd3895fdc482ac4de966
Tom Clark <thrclark@iu.edu>
2018-05-02 07:23:40 -0400

    updates

--- photos.php
+++ photos.php
@@ -17,7 +17,7 @@ echo $buffer;
 <meta name="description" content="Photo Gallery - Cool City Band at weddings, fundraisers, galas, special events and concerts featuring the best musicians and vocalists.">
 </head>
 
-<body>
+<body>	
 <!-- Navigation -->
 <?php include ('includes/navigation.php') ?>
 <section class="section1">
@@ -27,88 +27,8 @@ echo $buffer;
                 <h2 class="page-header">Photos</h2>
             </div>
             <div class="col-lg-12">
-            
-           <p> Browse pictures of the Cool City Band in action at events throughout the Midwest including gala fundraisers, concerts, weddings and anniversaries. You’ll see why the Cool City Band is considered one of the best live wedding bands as well as the premiere event band in the Midwest. <a href="contact.php">Contact us today</a> for more information.</p>
-            
-            
-            
-                <div class="well well-sm fotorama"   data-width="100%" data-ratio="800/600" data-navposition="top"  data-nav="thumbs" style="margin-top:30px"> 
-                
-                
-                
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics1.png"> 
-                
-                
-                       
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics2.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics3.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics4.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics5.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics6.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics7.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics8.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics9.png"> 
-                
-                     
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018pics10.png"><img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-005.png"><img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-007.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-008.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-009.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-010.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-012.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-013.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-014.png">
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-001.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-016.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-017.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-018.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-019.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-020.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-021.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-022.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-023.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-024.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-026.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-027.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-028.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-029.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-030.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-031.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-032.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-033.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-034.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-035.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-037.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-038.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-039.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-040.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-041.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-042.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-043.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-044.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-045.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-046.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-047.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-049.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-050.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-052.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-053.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-054.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-055.png"> 
-<img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/gallery-056.png"> </div>
+                <p> Browse pictures of the Cool City Band in action at events throughout the Midwest including gala fundraisers, concerts, weddings and anniversaries. You’ll see why the Cool City Band is considered one of the best live wedding bands as well as the premiere event band in the Midwest. <a href="contact.php">Contact us today</a> for more information.</p>
+                <div class="well well-sm fotorama"   data-width="100%" data-ratio="800/600" data-navposition="top"  data-nav="thumbs" style="margin-top:30px"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-1.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-2.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-3.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-4.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-5.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-6.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-7.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-8.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-9.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-10.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-11.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-12.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-13.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01a-14.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-1.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-2.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-3.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-4.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-5.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-6.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-7.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-8.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-9.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-10.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-11.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-12.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-13.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01b-14.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-1.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-2.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-3.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-4.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-5.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-6.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-7.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-8.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-9.png"> <img class="img-responsive" alt="Live performance with the Cool City Band" src="img/gallery/2018-05-01c-10.png"> </div>
             </div>
         </div>
     </div>
